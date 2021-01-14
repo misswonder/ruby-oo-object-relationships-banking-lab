38bef2d9f849bd33b94d3aeb2e536a4aef0628f4
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-14 17:17:05 -0600

    commit tree 799d58a8429f052ea4458b9cb5bb8bdd44ef5ee2

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -34,7 +34,7 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
-  
+
   def reverse_transfer
     if execute_transaction
       @sender.balance += @amount
