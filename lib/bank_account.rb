0abfc42eb7f9e6d573c1d79b2ab962a86c68cb7f
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-14 11:22:51 -0600

    relationship banking

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status, :deposit
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+    end
+
+    def deposit(deposit) 
+        @deposit = deposit   
+        #binding.pry
+        @balance += @deposit
+        #binding.pry 
+    end 
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end 
+
+    def 
+
 end
+
